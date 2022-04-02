1b0fe6191d19654b8fab09a322058155363dce88
hwimin-kim <gnlals7841@gmail.com>
2022-04-02 17:01:10 +0900

    POST 요청에 대한 UTF-8 인코딩 적용하여 한글깨짐 수정

---
+++ src/main/java/com/hwimin/web/NoticeReg.java
@@ -0,0 +1,31 @@
+package com.hwimin.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/notice-reg")
+public class NoticeReg extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest requset, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setCharacterEncoding("UTF-8");
+		requset.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		PrintWriter out = response.getWriter();
+
+		String title = requset.getParameter("title");
+		String content = requset.getParameter("content");
+		
+		out.print(title);
+		out.print(content);
+	}
+}
